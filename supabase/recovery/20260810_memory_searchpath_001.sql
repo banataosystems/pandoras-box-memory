b40161c03400f77f7e5a931c0aa639e57579b59b
Mark Johnson Banatao <lawbatalla@gmail.com>
2026-08-11 14:48:19 +0800

    recovery: preserve exact Memory search-path candidate

---
+++ supabase/recovery/20260810_memory_searchpath_001.sql
@@ -0,0 +1,5 @@
+-- MEMORY-SEARCHPATH-001 governed candidate
+ALTER FUNCTION public.set_updated_at() SET search_path = '';
+ALTER FUNCTION public.set_pandora_shadow_context_pack_updated_at() SET search_path = '';
+ALTER FUNCTION public.set_pandora_promotion_requests_updated_at() SET search_path = '';
+ALTER FUNCTION public.set_pandora_promotion_executions_updated_at() SET search_path = '';
